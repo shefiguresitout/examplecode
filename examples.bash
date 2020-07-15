fdac30f2e93c4d081b578df67b29f1cb705fdbbd
shefiguresitout <11182306+shefiguresitout@users.noreply.github.com>
2020-07-15 10:14:02 -0400

    improve if-elif-else-fi

--- examples.bash
+++ examples.bash
@@ -3,8 +3,13 @@ for file in *; do
   ls ${file}
 done
 
-if [ $? -eq 1 ]; then
-	echo "exit status is 1"
+if [ $? -eq 0 ]; then
+  echo "Success"
+elif [ $? -eq 1 ]; then
+  echo "exit status is 1"
 elif [ $? -gt 1 ]; then
-	echo "exit status -gt 1"
+  echo "exit status -gt 1"
+else
+  #should never get here
+  echo '$? is' $?
 fi
